0c1eb0b4aefaba7c2fe7419b982db341fe7efcdd
Raj <adiflickr3@gmail.com>
2022-08-23 15:12:49 +0530

    updating profile info

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .form import UserRegisterForm
+from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -22,6 +23,17 @@ def register(request):
 
 @login_required()
 def profile(request):
-    return render(request, "users/profile.html")
-
-
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f"Account updated...!!!!")
+            return redirect('profile')
+    user_form = UserUpdateForm(instance=request.user)
+    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {'user_form': user_form,
+               'profile_form': profile_form
+               }
+    return render(request, "users/profile.html", context)
