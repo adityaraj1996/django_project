f02487e19178cb914ba5d5588aea108793434bdb
Raj <adiflickr3@gmail.com>
2022-08-29 13:36:50 +0530

    adding pagination feature for the posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -13,9 +14,22 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'   # app/model_viewtype.html
-    context_object_name = 'posts'      # object list
+    template_name = 'blog/home.html'  # app/model_viewtype.html
+    context_object_name = 'posts'  # object list
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # app/model_viewtype.html
+    context_object_name = 'posts'  # object list
+    # ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -25,6 +39,7 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+
     # for populating the author as the current user
 
     def form_valid(self, form):
@@ -35,13 +50,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
+
     # for populating the author as the current user
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    def test_func(self):           # prevent other users to edit post
+    def test_func(self):  # prevent other users to edit post
         post = self.get_object()
         return post.author == self.request.user
 
@@ -50,7 +66,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
 
-    def test_func(self):           # prevent other users to edit post
+    def test_func(self):  # prevent other users to edit post
         post = self.get_object()
         return post.author == self.request.user
 
