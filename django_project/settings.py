ef1e3e8711ae7f7cffe4fc6dede6641ff7c11bca
Raj <adiflickrs3@gmail.com>
2022-09-06 00:52:19 +0530

    adding django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 import storages.backends.s3boto3
 
@@ -159,5 +160,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = "ap-south-1"
+
+django_heroku.settings(locals())
 
 
