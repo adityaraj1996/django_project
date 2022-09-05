bbd4492e65d18beb536354c7ee7d46b92c67d645
Raj <adiflickrs3@gmail.com>
2022-09-06 00:24:33 +0530

    configuring secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbt6a1q8=c5kb($6b6m(8=m)do7wj++ay&18=s*yf4)%=49_u*'
+# SECRET_KEY = 'django-insecure-rbt6a1q8=c5kb($6b6m(8=m)do7wj++ay&18=s*yf4)%=49_u*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-blog-apps.herokuapp.com']
 
 
 # Application definition
